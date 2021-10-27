40256357f8224bf4f77c172b33d09c9a44d6d4fd
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-27 12:40:54 -0700

    added button component, added button scss, added DayList component, added DayListItem component, added DayListItem scss, added DayListItem test

---
+++ src/components/DayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+
+
+function daySelect (day, selectedDay){
+  if(day === selectedDay){
+    return true;
+  }
+  return false;
+}
+
+export default function DayList(props) {
+  
+  const {days} = props;
+
+  console.log(days[1].id)
+
+  const outPut = days.map(dayItems => <DayListItem id={dayItems.id} name={dayItems.name} spots={dayItems.spots} selected={daySelect(dayItems.name, props.day)} setDay={props.setDay}/>)
+  
+   return (
+     <ul>
+       {outPut}
+     </ul>
+  ); 
+ }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,40 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+
+
+
+export default function DayListItem(props) {
+  
+
+  function formatSpots (spots) {
+    if(spots === 0){
+      return "no spots remaining"
+    }
+    if(spots === 1){
+      return "1 spot remaining"
+    }
+    if(spots >= 2 ){
+      return `${spots} spots remaining`
+    }
+  }
+
+  const dayClass = classNames("day-list__item", {
+    
+    "day-list__item--selected": props.selected === true,  
+    "day-list__item--full": props.spots === 0
+    
+  });
+ 
+ //console.log("kasjdhflsjkdhfgksdjlhf",props);
+
+  return (
+    <li onClick={() => props.setDay(props.name)}  className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">
+        
+        {formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
