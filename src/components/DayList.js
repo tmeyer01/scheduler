b90ff420483f9a44a9f29bb1a97891ede9bc9d60
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-27 20:15:03 -0700

    refactored code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -24,8 +24,9 @@ export default function DayList(props) {
         key={day.id}
         name={day.name}
         spots={day.spots}
-        selected={day.name === props.day}
-        setDay={()=>props.setDay(day.name)}
+        selected={day.name === props.value}
+        //setDay={()=>props.setDay(day.name)}
+        setDay={props.onChange}
       />
     )
   })
