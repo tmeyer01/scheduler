6c8e42abe1aef581239bbe0f60871d767874a157
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-27 20:38:58 -0700

    Added onClick image in Emptry.js & Added onAdd prop to Empty component in Stories index

--- stories/index.js
+++ stories/index.js
@@ -12,7 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header"
-
+import Empty from "components/Appointment/Empty"
 
 storiesOf("Appointment", module)
   .addParameters({
@@ -20,8 +20,8 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", ()=> <Header time="12pm" />);
-  
+  .add("Header", ()=> <Header time="12pm" />)
+  .add("Empty", ()=> <Empty onAdd={action("onAdd")}/>);
 const interviewer = {
   id: 1,
   name: "Sylvia Palmer",
