6d70dd38040ffd5be4e44954b46b9843589704be
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-27 20:56:21 -0700

    Added onEdit, Added onDelete props to Show component and in index stories

--- stories/index.js
+++ stories/index.js
@@ -11,8 +11,9 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header"
-import Empty from "components/Appointment/Empty"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show"
 
 storiesOf("Appointment", module)
   .addParameters({
@@ -21,7 +22,15 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", ()=> <Header time="12pm" />)
-  .add("Empty", ()=> <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", ()=> <Empty onAdd={action("onAddon")}/>)
+  .add("Show", ()=> (
+    <Show 
+      student="Lydia Miller-Jones"
+      interviewers={interviewers}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ));
 const interviewer = {
   id: 1,
   name: "Sylvia Palmer",
