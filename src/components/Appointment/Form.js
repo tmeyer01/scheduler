db51f605b110166cc2721ccdac8b78d18e104b55
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-29 16:02:08 -0700

    Added cancel and rest function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Button from "components/Button"
 import InterviewerList from 'components/InterviewerList'
 
+
 export default function Form(props) {
  
   const [student, setStudent] = useState(props.student || "");
@@ -10,15 +11,15 @@ export default function Form(props) {
  
  console.log("PROPS:", props)
 
-  function rest(){
+  function reset(){
     setStudent("");
     setInterviewer(null);
   }
   function cancel(){
-    rest();
+    reset();
     props.onCancel();
   }
- console.log("props", props)
+ //console.log("props", props)
   return (
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
