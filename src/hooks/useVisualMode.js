c8d5fa092f5c11c44acf761a8f34b48eefcd0c13
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-30 14:10:39 -0700

    Added react-hooks-testiing-library and Added custom hook for transition functionality

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    setHistory((prev) => {
+      if (replace === true) {
+        const newHistory = [...history];
+        newHistory.pop();
+        return [...newHistory, newMode];
+      }
+      return [...prev, newMode];
+    });
+    setMode(newMode);
+  };
+
+  const back = () => {
+    if (history.length < 1) {
+      return;
+    }
+    setHistory((prev) => {
+      prev.pop();
+      return [...prev];
+    });
+    setMode(history[history.length - 2]);
+  };
+  return { mode, transition, back };
+}
+
+
+
+
+
