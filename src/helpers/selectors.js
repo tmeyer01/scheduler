edcde61c6f93aa9fb57f5c392ddfb4526608a4e2
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-10-29 19:03:46 -0700

    Added getInterview function and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,26 @@
 export function getAppointmentsForDay(state, day) {
   const results = [];
-
-  const foundDay = state.days.find(d => d.name === day)
-  
-  if(!foundDay){
+  const foundDay = state.days.find((d) => d.name === day);
+  if (!foundDay) {
     return [];
   }
-  //iterating through foundDay apts and then pushing appointment into results array. 
-  for(const id of foundDay.appointments){
-    const apoint = state.appointments[id]
-    results.push(apoint)
+  //iterating through foundDay apts and then pushing appointment into results array.
+  for (const id of foundDay.appointments) {
+    const apoint = state.appointments[id];
+    results.push(apoint);
   }
-
   return results;
+}
 
+export function getInterview(state, interview) {
+  if(!interview){
+    return null;
+  }
+  
+  const id = interview.interviewer;
+  const interviewer = state.interviewers[id]
+
+
+
+ return {...interview,interviewer};
 }
