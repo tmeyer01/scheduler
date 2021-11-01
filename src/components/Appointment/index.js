b086998cc8f0891c2fc15227cebda880663a7e8d
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-11-01 16:34:17 -0700

    added transition to index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,44 @@ import "./styles.scss";
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode"
+//import getInterviewersForDay from "helpers/selectors"
 
 export default function Appointment(props) {
+  
+  const {id, bookInterview} = props
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+ 
+
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer
+    }
+    bookInterview(id, interview) 
+    return transition(SHOW);
+  }
+
+
+
+
   
   return (
     <article className="appointment">
       < Header time={props.time} />
-      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
-      {!props.interview && <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && ( <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+      )}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={()=> back()} onSave={save}/>}
+      
       
     </article>
   )
