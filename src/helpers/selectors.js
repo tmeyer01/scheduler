4bee715643ed9439820d8a215cb6a6abfea19d47
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-11-01 16:29:13 -0700

    created getInterviewerForDay function and created test for getInterviewerForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,18 +9,33 @@ export function getAppointmentsForDay(state, day) {
     const apoint = state.appointments[id];
     results.push(apoint);
   }
-  return results;
+  return results; 
 }
+
+
+
+export function getInterviewersForDay(state, day) {
+  const results = [];
+  const foundDay = state.days.find((d) => d.name === day);
+  if (!foundDay) {
+    return [];
+  }
+  //iterating through foundDay apts and then pushing appointment into results array.
+  for (const id of foundDay.interviewers) {
+    const apoint = state.interviewers[id];
+    results.push(apoint);
+  }
+  return results; 
+}
+  
+
 
 export function getInterview(state, interview) {
   if(!interview){
     return null;
-  }
-  
+  }  
   const id = interview.interviewer;
   const interviewer = state.interviewers[id]
-
-
 
  return {...interview,interviewer};
 }
