3078fd8165460f2d6910614d82367e5d15ec7718
ThomasMeyer <tmeyerwebdev@gmail.com>
2021-11-01 16:30:52 -0700

    Added onClick to Save button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -19,7 +19,7 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
- //console.log("props", props)
+
   return (
   <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
@@ -43,7 +43,7 @@ export default function Form(props) {
     <section className="appointment__card-right">
       <section className="appointment__actions">
         <Button danger onClick={cancel} >Cancel</Button>
-        <Button confirm >Save</Button>
+        <Button confirm onClick={()=>{props.onSave(student, interviewer)}}>Save</Button>
       </section>
     </section>
   </main>
